7a3b186957f34ed76bb1b6ce9c188c91fbad7c39
Heavensolder94 <mailvahanam.raoul@gmail.com>
2026-05-05 22:09:21 +0200

    search.js

--- api/ebay/search.js
+++ api/ebay/search.js
@@ -1,11 +1,11 @@
 export default function handler(req, res) {
   res.status(200).json({
     ok: true,
-    version: "RICHTIGE-SEARCH-DATEI-TEST",
+    version: "LOWERCASE-EBAY-SEARCH-TEST",
     query: req.query.q || "",
     items: [
       {
-        title: "TESTARTIKEL AUS DER RICHTIGEN SEARCH.JS",
+        title: "TESTARTIKEL AUS API/ebay/search.js",
         price: { value: "1.00", currency: "EUR" },
         condition: "Test",
         itemWebUrl: "https://www.ebay.de"
