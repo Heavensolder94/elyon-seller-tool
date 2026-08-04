864aedfbdb4562aadf82b0ecaf7ebdc8f2e42870
Heavensolder94 <mailvahanam.raoul@gmail.com>
2026-08-04 18:12:23 +0200

    chore: bust cache for seller operations suite

--- seller-runtime-loader.js
+++ seller-runtime-loader.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const VERSION = "order-center-20260804-1";
+  const VERSION = "seller-operations-20260804-1";
   const LEGACY_QUICKSTART_BRIDGE_FLAG = "__elyonModernQuickstartBridge";
   const loaded = new Map();
   const groupLoads = new Map();
